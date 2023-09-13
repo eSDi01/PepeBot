7b19d5141822b9c35277affd26146fae105040c8
eSDi <sdrobashko@mail.ru>
2023-09-13 23:11:01 +0600

    add help command

---
+++ plugins/help.py
@@ -0,0 +1,15 @@
+from telethon import events
+
+
+async def init(client) -> None:
+    @client.on(events.NewMessage(pattern=r'(?i)/help$'))
+    async def handler(event):
+        await client.send_message(
+            entity=event.peer_id,
+            message=f'/all - Тэгает всех пользователей канала. Доступно только админам.\n'
+                    f'/alladmins - Тэгает всех админов канала\n'
+                    f'/addGroupSchedule <Название группы> - '
+                    f'Привязывает чат к каналу для расписании. Доступно только админам.\n'
+                    f'/showSchedule - Показывает расписание на сегодня. Доступно только админам.\n'
+                    f'/showTomSchedule - Показывает расписание на завтра. Доступно только админам.\n'
+                    f'/deleteGroup - Отвязывает чат к каналу. Доступно только админам.')
